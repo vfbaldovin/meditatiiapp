a516546981261adfb141ab5668f26ccdb26eb366
Virgil <virgil-florentin.baldovin@expleogroup.com>
2020-09-23 18:46:42 +0300

    Initial commit

---
+++ src/main/java/com/app/meditatiiapp/MeditatiiappApplication.java
@@ -0,0 +1,13 @@
+package com.app.meditatiiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditatiiappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeditatiiappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/meditatiiapp/MeditatiiappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.meditatiiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditatiiappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
